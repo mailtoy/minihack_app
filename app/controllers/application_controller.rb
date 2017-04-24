0ab917dda6dd727334adda11565942faf59fb27b
Kanchanok <kanchanok.kan@ku.th>
2017-04-24 08:51:30 +0000

    Add a1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+
+protect_from_forgery with: :exception
+
+def hello
+
+render html: "Problem 1 of Activity 1."
+end
 end
